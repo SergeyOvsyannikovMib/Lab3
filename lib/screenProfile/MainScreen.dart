ca94260cf12394436925c9fb6818889136da81d4
Sergey Ovsyannikov <vivor2014@gmail.com>
2024-02-29 08:39:04 +0600

    мелочь

--- lib/screenProfile/MainScreen.dart
+++ lib/screenProfile/MainScreen.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:lab2_test/screenProfile/screenElements/TabBarMainScreen.dart';
 
 class MainScreen extends StatelessWidget {
-  const MainScreen({Key? key} ): super(key: key);
+  const MainScreen({super.key} );
 
   @override
   Widget build(BuildContext context) {
